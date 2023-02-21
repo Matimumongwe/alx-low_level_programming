d7aa7ad6a808bc4ac37933569090ce34cb6e90fa
Matimu Mongwe <matimu11mongwe@gmail.com>
2023-02-21 15:03:07 -0800

    task 0

--- 0-putchar.c
+++ 0-putchar.c
@@ -2,6 +2,7 @@
 
 /**
  * main - prints _putchar and a new line
+ * is written by mbah
  * Return: Always 0
  */
 int main(void)
@@ -13,6 +14,7 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
