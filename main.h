942f3e686931d9ee94e82c529fc09d939733712e
Matimu Mongwe <matimu11mongwe@gmail.com>
2023-03-08 05:50:08 -0800

    0-puts_recursion.c

--- main.h
+++ main.h
@@ -1,18 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-void print_alphabet(void);
-void jack_bauer(void);
-void print_alphabet_x10(void);
-void times_table();
-void print_to_98(int);
-void print_times_table(int);
-int _putchar(char);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int add(int n, int m);
-int print_last_digit(int n);
-
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 #endif
