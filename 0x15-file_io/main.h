8a61aab3535bb56b4fca00386e5de8638dbfc17a
root <matimu11mongwe@gmail.com>
2023-05-09 04:10:01 -0700

    0x15-file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 /*
- * File: holberton.h
- * Auth: Godswill Kalu
+ * File: ALX SE
+ * Auth: Matimu Mongwe
  * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
+ *       written in the 0x15-file_io directory.
  */
 
 #include <sys/types.h>
